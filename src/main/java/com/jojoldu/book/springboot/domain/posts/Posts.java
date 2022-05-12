52d68efded9f9791bc61283646dd7ba19aca1bfa
dongjun6343 <dongjun6343@naver.com>
2022-05-12 17:54:38 +0900

    등록/수정/조회 API 만들기

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -72,4 +72,9 @@ public class Posts {
         this.content = content;
         this.author = author;
     }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
