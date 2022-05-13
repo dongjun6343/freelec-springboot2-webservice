5bebc43ebd2793d8d779b205c2df27c972f6bb7a
dongjun6343 <dongjun6343@naver.com>
2022-05-13 14:26:43 +0900

    JPA Auditing으로 생성시간/수정시간 자동화하기.

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,6 +2,7 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  *  @SpringBootApplication : 프로젝트 최상단에 위치. (메인 클래스)
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *     SpringApplication.run : 내장 WAS 실행. (내장 WAS - 외부에 WAS를 두지 않고 애플리케이션을 실행할 때 내부 WAS를 실행한다.)
  *     스프링부트에서는 내장 WAS 사용을 권장함. Why? 언제 어디서나 같은 환경에서 스프링 부트를 배포할 수 있기 때문에.
  */
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
---
+++ src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package com.jojoldu.book.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+
+/**
+ * @MappedSuperclass : JPA Entity 클래스들이 BaseTimeEntity을 상속할 경우 필드에 createDate, modifiedDate 인식.
+ * @EntityListeners(AuditingEntityListener.class) : Auditing 기능 포함.
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
